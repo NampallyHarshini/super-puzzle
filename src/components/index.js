57587ea892144f075e882015b6f5aafe7be96e1a
NampallyHarshini <121306410+NampallyHarshini@users.noreply.github.com>
2022-12-23 21:59:24 +0530

    index.js

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Game from './Game';
+import Instruction from './Instructions';
+import Header from './Header';
+import Details from './Details';
+
+export { Game, Instruction, Header, Details };
